ca8c1a96f2c3b549938b68acb1473d2eb5fcfe63
Harneack <nickbudwal@hotmail.com>
2014-03-16 13:26:05 +0000

    first commit

---
+++ application/views/index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>My Portfolio</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600,700"         rel="stylesheet" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/config.js"></script>
+		<script src="js/skel.min.js"></script>
+    	<script type="text/javascript" src="js/jquery-1.2.6.pack.js"></script>
+	    <script type="text/javascript" src="js/script.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+	        <link rel="stylesheet" href="css/default.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+		<!--[if lte IE 7]><link rel="stylesheet" href="css/ie7.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Top</a></li>
+					<li><a href="#work">Me</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+
+		<!-- Home -->
+			<div class="wrapper wrapper-style1 wrapper-first">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="4u">
+							<span class="me image image-full"> <img src="images/me.jpg" alt="" /></span>
+						</div>
+						<div class="8u">
+							<header>
+								<h1>Hi. I'm <strong>Harneack Budwal</strong>.</h1>
+							</header>
+							<p>A <strong>Website Developer</strong> From Huddersfield,</p>
+							<p>Welcome To My <strong>Portfolio</strong>.</p>
+							<a href="#work" class="button button-big">Me and my skills</a>
+						</div>
+					</div>
+				</article>
+			</div>
+
+		<!-- Work -->
+			<div class="wrapper wrapper-style2">
+				<article id="work">
+					<header>
+						<h2>I'm a designer and website developer with a passion for clean code and elegant design.</h2>
+						<span> The passion for my work comes naturally as enjoy the development of websites and designing products for clients.</span><br/>
+					<span><strong>Professional Capabilities</strong><br/>
+					<ul>
+<li>High-quality front and back-end development for web sites.</li>
+<li>Modular, scalable, robust, and reusable code. </li>
+<li>Performance optimisation, usability, and accessibility. </li>
+<li>SEO using semantic HTML and Microformats. </li>
+<li>Site planning and architecture.</li>
+<li>User interface design.</li>
+</ul
+					
+					</span>
+					<span>  <strong>Below Are My Technical Skills</strong> </span>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="4u">
+								<section class="box box-style1">
+								<a href="" class="image image-full"><img src="images/js.png" alt="" /></a>
+								</section>
+							</div>
+							<div class="4u">
+								<section class="box box-style1">
+									<a href="" class="image image-full"><img src="images/php.png" alt="" /></a>
+								</section>
+							</div>
+							<div class="4u">
+								<section class="box box-style1">
+									<a href="" class="image image-full"><img src="images/ajax.png" alt="" /></a>
+								</section>
+							</div>
+						</div>
+					</div>
+					<footer>
+					<p>These skills have been developed during my university computing course and exceled in my personal time where I have experimented using each skill,<br/> to accomplish tasks and requirements for my projects. My skills are only the foundation of what I am capable of doing and over time they will be extended further. </p>
+					<div >
+					<section class="box box-style1">
+					<a href="Harneack Budwal CV.docx" target="Harneack Budwal CV.docx"><img src="images/cv.png" alt="CV" /></a><br/>
+					  <h2>Please Cick The Cv Image To Download</h2> 
+</section>
+					</div>
+						
+						<a href="#portfolio" class="button button-big">See some of my recent work</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Portfolio -->
+			<div class="wrapper wrapper-style3">
+				<article id="portfolio">
+					<header>
+						<h2>My Work.</h2>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="12u">
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u">
+								<article class="box box-style2">
+									<a href="" class="image image-full"><img src="images/portfolio01.png" alt="" /></a>
+									<h3>Maan Clothing</h3>
+									<span>  <strong>Description</strong> </span>
+									<p> A Online retail store which allows users to register, login and browse the items then add their desired items to the online basket where they will then be taken to a payment method for completion.</p>
+									<span>  <strong>Postition</strong> </span>
+									<p>
+									Group project (My role: Shopping cart, Payment Processor Integration, Responsive Design)
+									</p>
+									<span>  <strong>Skills</strong> </span>
+									<p>
+									HTML5, Css3, PHP, MySQL, Java Script And Twitter Bootstrap
+									</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box box-style2">
+									<a href="" class="image image-full"><img src="images/portfolio02.jpg" alt="" /></a>
+									<h3>YourPix</h3>
+									<span>  <strong>Description</strong> </span>
+								<p> A photo sharing site that allows users to create online profiles and upload images with options to manipulate the image and also share images with friends on social networking sites.
+									</p>
+									<span>  <strong>Postition</strong> </span>
+									<p>
+									Lead web developer/designer
+									</p>
+									<span>  <strong>Skills</strong> </span>
+									<p>
+									HTML5, Css3, PHP, MySQL, Java Script, Codeigniter, Ajax, Api, And Twitter Bootstrap
+									</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box box-style2">
+									<a href="" class="image image-full"><img src="images/portfolio03.jpg" alt="" /></a>
+									<h3>BPM</h3>
+									<span>  <strong>Description</strong> </span>
+								<p> A car sales website to act as an online showroom to and review site for customers, this site was also made with accessibility setting such as zoom and colour change for text. 
+									</p>
+									<span>  <strong>Postition</strong> </span>
+									<p>
+									Lead web developer/designer
+									</p>
+									<span>  <strong>Skills</strong> </span>
+									<p>
+									Css, PHP, MySQL and Java Script
+									</p>
+								</article>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u">
+								<article class="box box-style2">
+									<a href="http://flypixel.com/n33-pattern-set-1/3522389001865317" class="image image-full"><img src="images/portfolio04.jpg" alt="" /></a>
+								<h3>MOVIES 'R' US</h3>
+									<span>  <strong>Description</strong> </span>
+								<p> A movie review site with rss movie news feeds and movie review requests for customers feel that they are the main focus. google api's to identify imporatnt loacations.
+									</p>
+									<span>  <strong>Postition</strong> </span>
+									<p>
+									Personal Project  
+									</p>
+									<span>  <strong>Skills</strong> </span>
+									<p>
+									HTML5, Css3, PHP, MySQL, Java Script,  Ajax, Api, And Responsive Design(fluid Grids)
+									</p>
+								</article>
+							</div>
+						
+						</div>
+					</div>
+					<footer>
+
+						<a href="#contact" class="button button-big">Contact Me</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Contact -->
+			<div class="wrapper wrapper-style4">
+				<article id="contact" class="container small">
+				
+					<div>
+						<div class="row">
+							<div class="12u">
+	<header>
+						<h2>Want to hire me? Get in touch!</h2>
+
+					</header>
+
+<form method="post" action="sendEmail.php">
+<div id="container">
+
+	<div id="main">
+		<p><small>Name:</small> <input type="text" name="name" id="name" /></p>
+		<p><small>Email Address:</small> <input type="text" name="email" id="email" /></p>
+		<p><small>Questions/Comments:</small> <textarea name="comments" id="comments" rows="12"></textarea></p>	
+		<p><input type="submit" name="submit" id="submit" value="Send!" /></p>
+
+		<ul id="response" />
+	</div><!--end main -->
+	
+</div><!-- end container -->
+</form>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<hr />
+								<h3>Find me on ...</h3>
+								<ul class="social">
+									<li class="linkedin"><a href="#" class="fa fa-linkedin"><span>LinkedIn</span></a></li>
+									<li class="github"><a href="http://github.com/n33" class="fa fa-github"><span>Github</span></a></li>
+									<!--
+									<li class="rss"><a href="#" class="fa fa-rss"><span>RSS</span></a></li>
+									<li class="instagram"><a href="#" class="fa fa-instagram"><span>Instagram</span></a></li>
+									<li class="foursquare"><a href="#" class="fa fa-foursquare"><span>Foursquare</span></a></li>
+									<li class="skype"><a href="#" class="fa fa-skype"><span>Skype</span></a></li>
+									<li class="soundcloud"><a href="#" class="fa fa-soundcloud"><span>Soundcloud</span></a></li>
+									<li class="youtube"><a href="#" class="fa fa-youtube"><span>YouTube</span></a></li>
+									<li class="blogger"><a href="#" class="fa fa-blogger"><span>Blogger</span></a></li>
+									<li class="flickr"><a href="#" class="fa fa-flickr"><span>Flickr</span></a></li>
+									<li class="vimeo"><a href="#" class="fa fa-vimeo"><span>Vimeo</span></a></li>
+									-->
+								</ul>
+								<hr />
+							</div>
+						</div>
+					</div>
+					<footer>
+						<ul id="copyright">
+							<li>&copy; 2014 Harneack Budwal</li>
+
+						</ul>
+					</footer>
+				</article>
+			</div>
+
+
+	</body>
+</html>
---
+++ sendEmail.php
@@ -0,0 +1,59 @@
+<?php
+	
+	$name = trim($_POST['name']);
+	$email = $_POST['email'];
+	$comments = $_POST['comments'];
+	
+	$site_owners_email = 'nick@moviesrus.co.uk'; // Replace this with your own email address
+	$site_owners_name = 'Harneack Budwal'; // replace with your name
+	
+	if (strlen($name) < 2) {
+		$error['name'] = "Please enter your name";	
+	}
+	
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address";	
+	}
+	
+	if (strlen($comments) < 3) {
+		$error['comments'] = "Please leave a comment.";
+	}
+	
+	if (!$error) {
+		
+		require_once('phpMailer/class.phpmailer.php');
+		$mail = new PHPMailer();
+		
+		$mail->From = $email;
+		$mail->FromName = $name;
+		$mail->Subject = "Website Contact Form";
+		$mail->AddAddress($site_owners_email, $site_owners_name);
+		$mail->Body = $comments;
+		
+		// GMAIL STUFF
+		
+		$mail->Mailer = "";
+		$mail->Host = "";
+		$mail->Port ="" ;
+		$mail->SMTPSecure = ""; 
+		
+		$mail->SMTPAuth = false; // turn on SMTP authentication
+		$mail->Username = ""; // SMTP username
+		$mail->Password = ""; // SMTP password
+	
+		
+		$mail->Send();
+		
+		echo "<li class='success'> Thank You, " . $name . ". I've received your email. I'll be in touch as soon as possible! </li>";
+		
+	} # end if no error
+	else {
+
+		$response = (isset($error['name'])) ? "<li>" . $error['name'] . "</li> \n" : null;
+		$response .= (isset($error['email'])) ? "<li>" . $error['email'] . "</li> \n" : null;
+		$response .= (isset($error['comments'])) ? "<li>" . $error['comments'] . "</li>" : null;
+		
+		echo $response;
+	} # end if there was an error sending
+
+?>
